b3cbc6265e37550e9361c2e87a30fd47ba5c0d5b
sbogado <sebastian.bogado@snoopconsulting.com>
2015-12-16 14:07:17 -0300

    Se crea la estructura en gestion usuarios

--- pyme_managment/gestion_usuarios/src/main/java/ar/com/greenleave/pyme_managment/gestion_usuarios/TipoTelefono.java
+++ pyme_managment/gestion_usuarios/src/main/java/ar/com/greenleave/pyme_managment/gestion_usuarios/TipoTelefono.java
@@ -4,4 +4,26 @@ public class TipoTelefono {
 	private Long id;
 	private String tipoTelefono;
 	
+	
+	
+	
+	/**
+	 * 
+	 * Comienzo del bloque para los getters and setters
+	 * 
+	 */
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTipoTelefono() {
+		return tipoTelefono;
+	}
+	public void setTipoTelefono(String tipoTelefono) {
+		this.tipoTelefono = tipoTelefono;
+	}
+	
 }
