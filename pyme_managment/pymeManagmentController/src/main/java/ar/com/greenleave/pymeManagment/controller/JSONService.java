0aa6f9cd912a962b8e349bcc7973cdeae48b04f2
sbogado <sebastian.bogado@snoopconsulting.com>
2016-03-07 18:14:34 -0300

    Se cambian cosas de maven

--- pyme_managment/pymeManagmentController/src/main/java/ar/com/greenleave/pymeManagment/controller/JSONService.java
+++ pyme_managment/pymeManagmentController/src/main/java/ar/com/greenleave/pymeManagment/controller/JSONService.java
@@ -8,9 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import ar.com.greenleave.pymeManagment.model.gestionCliente.Country;
 import ar.com.greenleave.pymeManagment.model.service.GestionClienteManager;
 
