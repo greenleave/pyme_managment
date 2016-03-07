32fcb0417768563df12d88467e6149c5c9a8f0f9
sbogado <sebastian.bogado@snoopconsulting.com>
2016-03-07 10:19:57 -0300

    Se agrega un servicio

--- pyme_managment/pymeManagmentController/src/main/java/ar/com/greenleave/pymeManagment/controller/JSONService.java
+++ pyme_managment/pymeManagmentController/src/main/java/ar/com/greenleave/pymeManagment/controller/JSONService.java
@@ -7,9 +7,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import org.springframework.web.context.WebApplicationContext;
+
+import ar.com.greenleave.pymeManagment.model.gestionCliente.Country;
+import ar.com.greenleave.pymeManagment.model.gestionCliente.dao.impl.GestionClienteDaoImpl;
+import ar.com.greenleave.pymeManagment.model.service.GestionClienteManager;
+
 @Path("/json/product")
 public class JSONService {
-
+	
+	private WebApplicationContext context;
+	private DozerBeanMapper mapper;
+	private GestionClienteManager gestionCliente;
+	
+	public JSONService() {
+		context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		mapper = (DozerBeanMapper) context.getBean("mapperDozer");
+		gestionCliente = (GestionClienteManager) context.getBean("infoClienteService");
+	}
+	
+			
+			
 	@GET
 	@Path("/get")
 	@Produces("application/json")
@@ -28,6 +46,16 @@ public class JSONService {
 	@Consumes("application/json")
 	public Response createProductInJSON(Product product) {
 		String result = "Product created : " + product;
+		return Response.status(201).entity(result).build();
+
+	}
+	
+	@POST
+	@Path("/post")
+	@Consumes("application/json")
+	public Response saveContry(Country contry) {
+		
+//		String result = "Product created : " + product;
 		return Response.status(201).entity(result).build();
 
 	}
