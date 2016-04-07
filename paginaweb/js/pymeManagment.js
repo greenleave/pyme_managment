c49b713b5e50df1ceff48118cf76cd78f0de9840
sbogado <seebogado@hotmail.com>
2016-04-07 00:57:37 -0300

    QUiero llorar

--- paginaweb/js/pymeManagment.js
+++ paginaweb/js/pymeManagment.js
@@ -1,17 +1,27 @@
-/**
-	Basicamente aca empiezo a definir la app y a donde iria cada cosa. 
-	Es decir si llega un pedido sobre una pagina en especial primero angular
-	va a  ver si ya tiene definida alguna accion para la misma
-**/
-var app = angular.module('pymeManagmentApp');
-app.config(function($routeProvider){
+var pymeManagmentApp = angular.module('pymeManagmentApp', [ 'ngRoute' ])
+/*
+var pymeManagmentApp = angular.module('pymeManagmentApp', [ 'ngRoute' ])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        when('/',{
+		templateUrl: '../html/login.html'
+		}).otherside({
+			redirectTo: 'www.google.com.ar'
+		});
+});
+*/
+/*
+var app = angular.module('pymeManagmentApp',[ 'ngRoute' ]);
+app.config(['$routeProvider', function($routeProvider){
 	$routeProvider.when('/',{
 		templateUrl: 'login.html'
 	}).otherside({
 		redirectTo: '/'
 	});
-});
+}]);
+*/
 
+/*
 app.controller('loginController', function($scope){
     $scope.submit = function(){
     	var uname = $scope.username;
@@ -19,3 +29,9 @@ app.controller('loginController', function($scope){
     	alert("Hola");
     });
   });
+ */ 
+/**
+	Basicamente aca empiezo a definir la app y a donde iria cada cosa. 
+	Es decir si llega un pedido sobre una pagina en especial primero angular
+	va a  ver si ya tiene definida alguna accion para la misma
+**/
