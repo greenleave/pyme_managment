70da287721e8a1d925da8a83a4e933f4f98dde27
sbogado <seebogado@hotmail.com>
2016-04-19 01:59:21 -0300

    problema con el import del applicationContext. Si soluciono eso, estaria un paso mas cerca.

--- pyme_managment/pymeManagmentWeb/src/main/java/ar/com/greenleave/pyme_managment/pymeManagmentWeb/controller/impl/GestionPymeControllerImpl.java
+++ pyme_managment/pymeManagmentWeb/src/main/java/ar/com/greenleave/pyme_managment/pymeManagmentWeb/controller/impl/GestionPymeControllerImpl.java
@@ -4,4 +4,154 @@ import ar.com.greenleave.pyme_managment.pymeManagmentWeb.controller.GestionPymeC
 
 public class GestionPymeControllerImpl implements GestionPymeController {
 
+	@Override
+	public String getClients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void getClient(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createClient(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCliente(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCliente(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getPersonas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPersona(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void crearPersona(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarPersona(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarPersona(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getCountries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getContry(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createCountry(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCountry(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCountry(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTypesAdress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeAdress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createTypeAdress(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTypeAdress(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTypeAdress(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTypesDocument() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeDocument(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createTypeDocument(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTypeDocument(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTypeDocument(String json) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
