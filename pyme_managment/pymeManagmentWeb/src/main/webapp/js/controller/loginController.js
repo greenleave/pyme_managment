7f35d7a68e35d5c20cf7c463876a399307451c2b
sbogado <sebastian.bogado@snoopconsultin.com>
2016-05-10 17:39:57 -0300

    Esta re inestable todo y vuela al joraca absolutamente todo

--- pyme_managment/pymeManagmentWeb/src/main/webapp/js/controller/loginController.js
+++ pyme_managment/pymeManagmentWeb/src/main/webapp/js/controller/loginController.js
@@ -1,18 +1,42 @@
 angular.module('mainApp').controller('loginController', mainLoginController);
-function mainLoginController($scope, loginService) {
-	$scope.login = function(){
+//Le inyecto como dependencia $location y loginService (es mi servicio)
+mainLoginController.$inject = ['$location', 'loginService'];
+function mainLoginController($scope, $location, loginService) {
+	var objectJs=this;	
+	objectJs.login = function(){
 		// var datos = loginService.getDatos($scope.userName, $scope.password);
-		var datos = loginService.loginUser('as','12345678a');
+		var datos = loginService.loginUser(objectJs.userName,objectJs.password);
 		console.log(datos);
 		datos.then(function(data){
 			if(data==""){
 				alert("El usuario o el password es invalido");
 			}
 			else{
-				$window.location.href="/dashboard";
+				alert("El usuario es valido y no se por que");
 			}
+		}),datos.error(function(error){
+			alert("Se a producido un error al conectarse con el server");
 		});
-		//Si da error en la conexiÃ³n lo redirijo a otro lugar del mundo
-
+		//Si da error en la conexiÃƒÂ³n lo redirijo a otro lugar del mundo
 	}
 }
+
+
+// angular.module('mainApp').controller('loginController', mainLoginController);
+// function mainLoginController($scope, loginService) {
+// 	$scope.login = function(){
+// 		// var datos = loginService.getDatos($scope.userName, $scope.password);
+// 		var datos = loginService.loginUser('as','12345678a');
+// 		console.log(datos);
+// 		datos.then(function(data){
+// 			if(data==""){
+// 				alert("El usuario o el password es invalido");
+// 			}
+// 			else{
+// 				$window.location.href="/dashboard";
+// 			}
+// 		});
+// 		//Si da error en la conexiÃ³n lo redirijo a otro lugar del mundo
+
+// 	}
+// }
--- pyme_managment/pymeManagmentWeb/src/main/webapp/js/services/loginService.js
+++ pyme_managment/pymeManagmentWeb/src/main/webapp/js/services/loginService.js
@@ -1,14 +1,18 @@
 angular.module('mainApp').factory('loginService', loginService);
-
+loginService.$inject = ['$http', '$rootScope'];
 function loginService($http,$q){
 
-	var pp = 	{
-					loginUser:loginUser
-				}
-	return pp;
+	var service = 	{
+						loginUser:loginUser,
+						setCredentials: setCredentials,
+						clearCredentials: clearCredentials
+					}
+	return service;
 
 // a lo que esta declarado como .then o ,function(error) se lo llama promesas. Eso es lo que returna el deferred.promise
-	function loginUser(){
+	function loginUser(userName, userPassword){
+		alert(userName);
+		alert(userPassword);
 		var deferred = $q.defer();
 		$http({
 				method: 'POST',
@@ -22,4 +26,41 @@ function loginService($http,$q){
 			});
 		return deferred.promise;		
 	}
+
+	function clearCredentials(){
+		alert("clearCredentials");	
+	}
+	
+	function setCredentials(){
+		alert("setCredentials");
+	}
+
 }
+
+
+
+// angular.module('mainApp').factory('loginService', loginService);
+
+// function loginService($http,$q){
+
+// 	var pp = 	{
+// 					loginUser:loginUser
+// 				}
+// 	return pp;
+
+// // a lo que esta declarado como .then o ,function(error) se lo llama promesas. Eso es lo que returna el deferred.promise
+// 	function loginUser(){
+// 		var deferred = $q.defer();
+// 		$http({
+// 				method: 'POST',
+// 				url: 'http://localhost:8080/pymeManagmentWeb/app/userCenter/login',
+// 				data: "{'userName': 'as', 'password': '12345678a' }",
+// 				headers: "{'Content-Type': 'application/json'}"
+// 			}).then(function(success){
+// 				deferred.resolve(success.data);
+// 			},function(error){
+// 				deferred.reject(error); 
+// 			});
+// 		return deferred.promise;		
+// 	}
+// }
