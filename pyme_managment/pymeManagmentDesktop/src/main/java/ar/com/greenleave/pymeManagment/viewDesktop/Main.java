21314a59028f59a153e06c121dcf5388a17fc7d9
sbogado <sebastian.bogado@snoopconsultin.com>
2016-05-24 17:49:04 -0300

    Commiteo esta parte de la pantalla

--- pyme_managment/pymeManagmentDesktop/src/main/java/ar/com/greenleave/pymeManagment/viewDesktop/Main.java
+++ pyme_managment/pymeManagmentDesktop/src/main/java/ar/com/greenleave/pymeManagment/viewDesktop/Main.java
@@ -1,5 +1,25 @@
 package ar.com.greenleave.pymeManagment.viewDesktop;
 
-public class Main {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.LabelBuilder;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Label label = LabelBuilder.create().text("Maven + JavaFX = true").alignment(Pos.CENTER).build();
+		Scene scene = new Scene(label, 200, 100);
+		stage.setScene(scene);
+		stage.show();
+
+	}
 
 }
