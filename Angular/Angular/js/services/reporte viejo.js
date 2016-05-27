1d215690795d7b75a8bcfb4410e00c86f8bbd3a1
sbogado <sebastian.bogado@snoopconsultin.com>
2016-05-27 15:57:46 -0300

    Hice mierda todo angular

--- Angular/Angular/js/services/reporte viejo.js
+++
@@ -1,9 +0,0 @@
-app.factory('informeService', ['$http', function($http) { 
-  return $http.get('http://localhost:8080/informeWS/service/json/getDatos') 
-            .success(function(data) { 
-              return data; 
-            }) 
-            .error(function(err) { 
-              return err; 
-            }); 
-}]);
