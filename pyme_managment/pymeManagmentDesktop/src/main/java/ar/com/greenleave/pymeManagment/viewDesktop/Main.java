7b34c2051e18879b4877e4c4490e01dec2c582fc
sbogado <sbogado@tecnosoftware.com>
2016-06-27 10:48:02 -0300

    Problemas al levantar la aplicacion visual

--- pyme_managment/pymeManagmentDesktop/src/main/java/ar/com/greenleave/pymeManagment/viewDesktop/Main.java
+++ pyme_managment/pymeManagmentDesktop/src/main/java/ar/com/greenleave/pymeManagment/viewDesktop/Main.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -17,20 +18,21 @@ public class Main extends Application {
 		launch(args);
 	}
 
-	public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
 		log.info("Starting Hello JavaFX and Maven demonstration application");
+
 		String fxmlFile = "/fxml/hello.fxml";
 		log.debug("Loading FXML for main view from: {}", fxmlFile);
-		FXMLLoader loader = new FXMLLoader();
-		Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-		log.debug("Showing JFX scene");
-		Scene scene = new Scene(rootNode, 400, 200);
-		scene.getStylesheets().add("/styles/styles.css");
-
-		stage.setTitle("Pyme Managment v1.0");
-		stage.setScene(scene);
-		stage.show();
+		try {
+			AnchorPane root = (AnchorPane) FXMLLoader.load(Main.class.getResource("/fxml/hello.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Bluerift Timeline");
+			primaryStage.show();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
